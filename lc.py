b4c35a3a45b62708cb5705a1f2ce8a488b6196f6
LidiaRR <lidia.roro3142@gmail.com>
2024-09-13 16:28:43 +0200

    delete commented code

--- lc.py
+++ lc.py
@@ -22,12 +22,6 @@ def mida(a: Arbre) -> int:
             return 0
         case Node(x,L):
             return 1 + reduce(lambda acc,y: acc+y, L)
-        #case Node(x, e, d):
-        #    return 1 + mida(e) + mida(d)
-
-#t = Node(1,Node(2,Buit(),Buit()),Node(3,Buit(),Buit()))
-
-#print('mida:', mida(t))
 
 @dataclass
 class App:
@@ -48,41 +42,10 @@ class TreeVisitor(lcVisitor):
 
     def visitParentesis(self,ctx):
         [op1,terme,op2] = list(ctx.getChildren())
-        # auxTree = self.tree
-        # for i in range(0,self.level):
-        #     auxTree = auxTree.child[-1]
-        # self.level += 1
-        # auxTree.child = ['(', self.visit(terme), ')']
-        # self.level -= 1
-
-    # def visitAplicacio(self,ctx):
-    #     [terme1,terme2] = list(ctx.getChildren())
-    #     auxTree = self.tree
-    #     for i in range(0,self.level):
-    #         auxTree = auxTree.child[-1]
-    #     self.level += 1
-    #     auxTree.child = [self.visit(terme1), self.visit(terme2)]
-    #     self.level -= 1
-
-    # def visitAbs(self,ctx):
-    #     [op1,lletra,op2,terme] = list(ctx.getChildren())
-    #     auxTree = self.tree
-    #     for i in range(0,self.level):
-    #         auxTree = auxTree.child[-1]
-    #     self.level += 1
-    #     auxTree.child = [op1.getText(), lletra.getText(), op2.getText(), self.visit(terme)]
-    #     self.level -= 1
 
     def visitLetra(self,ctx):
         [v] = list(ctx.getChildren())
         return Var(getText())
-    
-    # def visitLletra(self,ctx):
-    #     [lletra] = list(ctx.getChildren())
-    #     auxTree = self.tree
-    #     for i in range(0,self.level):
-    #         auxTree = auxTree.child[-1]
-    #     auxTree.child = [lletra.getText()]
 
 input_stream = InputStream(input('? '))
 lexer = lcLexer(input_stream)
